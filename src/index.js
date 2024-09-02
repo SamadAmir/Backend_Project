c68c4ad2e501a56dfc933e0ae36cd4f5465c8bf5
Samad Amir <samadaamir145@gmail.com>
2024-09-02 09:38:18 +0500

    MongoDB connection

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//require('dotenv').config({path:'./env'})
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+
+dotenv.config({path:'./env'})
+
+connectDB()
+
+
+
+
+
+
+
+
+//Connection with iffi an alternative method
+// ( async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERR: ",error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("Error: ",error)
+//         throw error
+//     }
+// })()
+
